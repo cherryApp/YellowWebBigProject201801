abbba48ff5ec9da75f77402c492e3b9295ca0683
Szász Kinga <kingus77@gmail.com>
2018-03-30 00:01:36 +0200

    Feat: dataClasses

---
+++ src/app/dataClasses.ts
@@ -0,0 +1,36 @@
+export class Iroda {
+    constructor(
+        city: string,
+        irodaId: number
+    ) { }
+}
+
+export class Agent {
+    constructor(
+        agentId: number,
+        agentName: string,
+        email?: string,
+        phone?: string
+    ) { }
+}
+
+export class LandLord {
+    constructor(
+        landlordId: number,
+        name: string,
+        realEstateId: number,
+        phone?: string
+    ) { }
+}
+
+export class RealEstate {
+    constructor(
+        address: string,
+        agentId: number,
+        area: number,
+        id: number,
+        margin: number,
+        price: number,
+        status: string
+    ) { }
+}
