60f0141545ddd9309ce521bc7300671aebc67ce1
Doni Pataki <yellow.pataki.doni@gmail.com>
2018-03-29 22:24:46 +0200

    doniP navbar design idea+ routerLinkActive: class.active

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -9,9 +9,9 @@ export class NavBarComponent implements OnInit {
 
   menuList: Array<{ label: string, url: string }> = [
     { label: 'Admin', url: '/login' },
-    { label: 'Customers', url: '/products' },
+    { label: 'Customers', url: '/customers' },
     { label: 'Products', url: '/products' },
-    { label: 'Orders', url: '/products' }
+    { label: 'Orders', url: '/orders' }
   ]
   constructor() { }
 
