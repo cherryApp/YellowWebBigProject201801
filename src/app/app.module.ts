59fc44033c3042cd8410110745db3a53334add14
Szász Kinga <kingus77@gmail.com>
2018-03-29 19:25:58 +0200

    Feat:basic routing working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,41 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AngularFireModule } from 'angularfire2';
 import { environment } from '../environments/environment';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { StudentComponent } from './student/student.component';
+
 import { AngularFireAuth } from 'angularfire2/auth';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { NavComponent } from './nav/nav.component';
 
+import { OfficesComponent } from './offices/offices.component';
+import { AgentsComponent } from './agents/agents.component';
+import { EstatesComponent } from './estates/estates.component';
+import { CustomersComponent } from './customers/customers.component';
+
+const appRoutes: Routes = [
+  { path: '', component: OfficesComponent, pathMatch: 'full' },
+  { component: OfficesComponent, path: 'offices' },
+  { component: AgentsComponent, path: 'agents' },
+  { component: EstatesComponent, path: 'estates' },
+  { component: CustomersComponent, path: 'customers' },
+];
+
 @NgModule({
   declarations: [
     AppComponent,
-    StudentComponent,
-    NavComponent
+    NavComponent,
+    CustomersComponent,
+    EstatesComponent,
+    AgentsComponent,
+    OfficesComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    AngularFireModule.initializeApp(environment.firebase)
+    AngularFireModule.initializeApp(environment.firebase),
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [
     AngularFireAuth,
