e7996ea8250c099f98a3e60b6c98bbd148be282a
peterciprian <35760611+peterciprian@users.noreply.github.com>
2018-03-30 15:25:49 +0200

    login beépítve

---
+++ app.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+
+import { AngularFireAuth } from 'angularfire2/auth';
+
+import { MedicinesService } from './medicines.service';
+import { PensionersService } from './pensioners.service';
+import { NursesService } from './nurses.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+  loginData: { email: string, pass: string } = {
+    email: "",
+    pass: ""
+  };
+  user: any;
+
+
+
+  constructor(
+    private afAuth: AngularFireAuth,
+    private mService: MedicinesService,
+    private pService: PensionersService,
+    private nService: NursesService,
+  ) {
+
+  }
+  ngOnInit() {
+    this.afAuth.authState.subscribe(
+      user => this.user = user,
+      error => console.error(error)
+    );
+  }
+
+  login(): void {
+    this.afAuth.auth.signInWithEmailAndPassword(
+      this.loginData.email,
+      this.loginData.pass
+    ).then(
+      value => console.log(value),
+      error => console.error(error)
+    );
+  }
+
+  title = 'app';
+}
