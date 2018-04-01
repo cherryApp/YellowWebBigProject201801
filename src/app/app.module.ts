b9ff908c6306fc727b5fa81f5b8c9a4c5b64c769
Hajkó Balázs <bhajko@gmail.com>
2018-04-01 14:32:46 +0200

    ...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,6 @@ import { AppComponent } from './app.component';
 import { StudentComponent } from './student/student.component';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { AngularFireDatabase } from 'angularfire2/database';
-
-import { AngularFirestoreModule } from 'angularfire2/firestore';
-import { AngularFireStorageModule } from 'angularfire2/storage';
-import { AngularFireAuthModule } from 'angularfire2/auth';
-
 import { UsersComponent } from './users/users.component';
 import { VehiclesComponent } from './vehicles/vehicles.component';
 import { DriversComponent } from './drivers/drivers.component';
@@ -42,10 +37,7 @@ const routes: Routes = [
     BrowserModule,
     FormsModule,
     AngularFireModule.initializeApp(environment.firebase),
-    RouterModule.forRoot(routes),
-    AngularFirestoreModule,
-    AngularFireAuthModule,
-    AngularFireStorageModule
+    RouterModule.forRoot(routes)
   ],
   providers: [
     AngularFireAuth,
