cdaf205d1f097e5dd3ce52c23a80167564e1996f
Hajkó Balázs <bhajko@gmail.com>
2018-04-01 16:41:40 +0200

    users tábla kész

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -16,10 +16,70 @@ export class UsersComponent implements OnInit {
   item: Observable<any>;
   tableData: Array<any> = [];
   newRow: any = {};
+  keys: Array<string> = [
+    "city",
+    "email",
+    "id",
+    "name"
+  ];
+  lastKey: string = "";
+  sorts: any = {};
+  order: number = 1;
+  currentData: any;
 
-  constructor() { }
+  constructor(private db: AngularFireDatabase) {
+
+    for (let k of this.keys) {
+      this.sorts[k] = {};
+    }
+
+    this.itemRef = db.object('users');
+
+    this.itemRef.valueChanges().subscribe(values => {
+
+      this.tableData = [];
+
+      for (var k in values) {
+        this.tableData.push({
+          key: k,
+          data: values[k]
+        });
+      }
+    });
+  }
 
   ngOnInit() {
+  }
+
+  sort(key): void {
+    for (var k in this.sorts) {
+      this.sorts[k] = "";
+    }
+    if (this.lastKey == key) {
+      this.order *= -1;
+    } else {
+      this.order = 1;
+    }
+    this.sorts[key] = this.order == -1 ? 'up' : 'down';
+
+    this.lastKey = key;
+    this.tableData.sort((a, b) => {
+      return a.data[key].toString().localeCompare(b.data[key].toString()) * this.order;
+    });
+  }
+
+  dataUpdate(row): void {
+    this.db.object('users/' + row.key).update(row.data);
+  }
+
+  dataDelete(key: string): void {
+    this.db.object('users/' + key).remove();
+  }
+
+  dataAdd(record: any) {
+    this.db.list('users').push(record).then(
+      r => this.newRow = {}
+    );
   }
 
 }
