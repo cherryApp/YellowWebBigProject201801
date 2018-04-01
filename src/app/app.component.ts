5685455fac8cea0f961396214b55c7ac6d3c2c77
Hajkó Balázs <bhajko@gmail.com>
2018-04-01 21:05:40 +0200

    ....

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,38 @@
 import { Component } from '@angular/core';
-
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
+import { AngularFireDatabase, AngularFireObject } from 'angularfire2/database';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  loginData: { email: string, pass: string } = {
+    email: "",
+    pass: ""
+  };
+  user: any;
+  constructor(
+    private afAuth: AngularFireAuth,
+    private db: AngularFireDatabase
+  ) { }
 
+  ngOnInit() {
+    this.afAuth.authState.subscribe(
+      user => this.user = user,
+      error => console.error(error)
+    );
+  }
+
+  login(): void {
+    this.afAuth.auth.signInWithEmailAndPassword(
+      this.loginData.email,
+      this.loginData.pass
+    ).then(
+      value => console.log(value),
+      error => console.error(error)
+    );
+  }
 
 }
