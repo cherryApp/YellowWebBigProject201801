1255b61054286432873a861f66dda867cb1eb526
Oberth Zsigmond <oberthzsigmond@gmail.com>
2018-04-01 21:03:22 +0200

    login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  loginData: { email: string, pass: string } = {
+    email: "",
+    pass: ""
+  };
+  user: any;
+  constructor(
+    private afAuth: AngularFireAuth,
+  ) {
 
+  }
+
+  login(): void {
+    this.afAuth.auth.signInWithEmailAndPassword(
+      this.loginData.email,
+      this.loginData.pass
+    ).then(
+      value => console.log(value),
+      error => console.error(error)
+    );
+  }
+  ngOnInit() {
+    this.afAuth.authState.subscribe(
+      user => this.user = user,
+      error => console.error(error)
+    );
+  }
 
 }
